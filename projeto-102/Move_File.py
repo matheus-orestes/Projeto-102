10deb786bde6babacfa60e8e6ca7afff22919350
matheus-orestes <100971661+matheus-orestes@users.noreply.github.com>
2023-02-16 18:54:45 -0300

    Add files via upload

---
+++ projeto-102/Move_File.py
@@ -0,0 +1,35 @@
+import os
+import shutil
+
+from_dir = "C:\Users\Matheus\Downloads"
+to_dir   = "C:/Byjus/imagensbaixadas"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+
+for file_name in list_of_files:
+
+    name, extension = os.path.splitext(file_name)
+    
+
+    if extension == '':
+        continue
+    if extension in ['.txt', '.pdf', '.doc', '.docx']:
+
+        path1 = from_dir + '/' + file_name                         
+        path2 = to_dir + '/' + "Arquivos_Documentos"                   
+        path3 = to_dir + '/' + "Arquivos_Documentos" + '/' + file_name 
+        
+        
+        if os.path.exists(path2):
+          print("Movendo " + file_name + ".....")
+
+        
+        
+          shutil.move(path1, path3)
+
+        else:
+          os.makedirs(path2)
+          print("Movendo " + file_name + ".....")
+          shutil.move(path1, path3)
